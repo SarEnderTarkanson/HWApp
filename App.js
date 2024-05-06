f7f0e5c59904c7d68f478e0e590d08381a326702
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-06 02:46:29 +0200

    added the first reuseable component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import MyText from './components/MyText/MyText';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Hi, I just completed my first React Native Assignment</Text>
+      <MyText />
     </SafeAreaView>
   );
 };
