0c6328f62b23f5779689b1201c5046d2c48db1e8
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-07 03:05:35 +0200

    added a useEffect Hook

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-import MyText from './components/MyText/MyText';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+//import MyText from './components/MyText/MyText';
 
 const App = () => {
+  const [text, setText] = useState(0);
+  useEffect(() => {
+    console.log(text);
+  }, [text]);
+
+  useEffect(() => {
+    console.log('The component has rendered!');
+  }, []);
   return (
     <SafeAreaView>
-      <MyText />
+      <Text onPress={() => setText(text + 1)}>{text}</Text>
     </SafeAreaView>
   );
 };
