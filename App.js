ebd97a5868aeecaeeef1a7a61cadd5eb388cbf22
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-11 02:21:13 +0200

    added my first class based component

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import {SafeAreaView, Button, Text} from 'react-native';
-import useToggle from './customHooks/useToggle';
+import React, {Component} from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
-const App = () => {
-  const [isOn, toggleIsOn] = useToggle(false);
-  return (
-    <SafeAreaView>
-      <Text>{isOn ? 'ON' : 'OFF'}</Text>
-      <Button title={'Toggle'} onPress={toggleIsOn} />
-    </SafeAreaView>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <SafeAreaView>
+        <Text
+          onPress={() => {
+            this.setState({name: 'Alpy'});
+          }}>
+          Hello, {this.state?.name} !
+        </Text>
+      </SafeAreaView>
+    );
+  }
+}
 
 export default App;
