77f644ad9b98cdf5e520a11c9221835ecc1b7305
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-13 03:38:44 +0200

    Simple app that displays user name and age

--- App.js
+++ App.js
@@ -1,39 +1,76 @@
-/* eslint-disable react/self-closing-comp */
-/* eslint-disable react-native/no-inline-styles */
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  Text,
+  Pressable,
+  Switch,
+  View,
+  Alert,
+} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState(' ');
+  const [age, setAge] = useState(' ');
+  const [showUserInfo, setSHowUserInfo] = useState(false);
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'white',
-        flexDirection: 'column',
-        justifyContent: 'space-around',
-        alignItems: 'baseline',
-      }}>
-      <View
-        style={{
-          backgroundColor: 'yellow',
-          width: 50,
-          height: 50,
-        }}></View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          width: 50,
-          height: 50,
-        }}></View>
-      <View
-        style={{
-          backgroundColor: 'black',
-          width: 50,
-          height: 50,
-        }}></View>
-    </View>
+    <SafeAreaView style={{backgroundColor: '#efdb96', flex: 1}}>
+      <ScrollView>
+        <TextInput
+          style={styles.ImputContainer}
+          placeholder="please enter your name"
+          onChangeText={value => setName(value)}
+        />
+        <TextInput
+          style={styles.ImputContainer}
+          placeholder="please enter your age"
+          onChangeText={value => setAge(value)}
+          keyboardType="numeric"
+        />
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <Switch
+            value={showUserInfo}
+            onValueChange={() =>
+              setSHowUserInfo(previousState => !previousState)
+            }
+          />
+          <Text>show my info </Text>
+        </View>
+        <Pressable
+          style={styles.button}
+          onPress={
+            showUserInfo
+              ? () => Alert.alert('Hi ' + name + ' your age is ' + age)
+              : () => Alert.alert('your informations are confidential')
+          }>
+          <Text style={{fontSize: 20}}> enter </Text>
+        </Pressable>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  ImputContainer: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+    backgroundColor: '#eae6da',
+  },
+
+  button: {
+    backgroundColor: '#84a295',
+    padding: 10,
+    borderRadius: 5,
+    marginLeft: 100,
+    marginRight: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
