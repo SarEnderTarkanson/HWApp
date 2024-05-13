d22fd21ef8084ccc626ded299a6a9e96e333d95a
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-13 02:29:35 +0200

    Pressable Component experimentation

--- App.js
+++ App.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, Image} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  TextInput,
+  Pressable,
+  Text,
+} from 'react-native';
 
 const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <SafeAreaView>
-      <ScrollView
-        onScroll={() => {
-          console.log('scrolling');
-        }}
-        contentContainerStyle={{backgroundColor: 'red', height: 600}}
-        showsHorizontalScrollIndicator={false}
-        horizontal={true}
-        showsHorizontalScrollIndicator={true}>
-        <Image
-          source={require('./assets/images/cake.png')}
-          style={{height: 500, width: 500}}
+      <ScrollView>
+        <TextInput
+          keyboardType={'email-address'}
+          style={{borderWidth: 1, borderRadius: 4, padding: 10}}
+          placeholder={'Please enter your email here'}
+          onChangeText={value => setEmail(value)}
         />
-        <Image
-          source={require('./assets/images/cake.png')}
-          style={{height: 500, width: 500}}
+        <TextInput
+          secureTextEntry={true}
+          style={{borderWidth: 1, borderRadius: 4, padding: 10}}
+          placeholder={'Please enter your password here'}
+          onChangeText={value => setPassword(value)}
         />
-        <Image
-          source={require('./assets/images/cake.png')}
-          style={{height: 500, width: 500}}
-        />
+        <Pressable
+          style={[
+            {backgroundColor: 'black'},
+            (!email || !password) && {opacity: 0.5},
+          ]}
+          disabled={!email || !password}
+          onPress={() => {
+            console.log('clicked!');
+            console.log(email, password);
+          }}>
+          <Text style={{color: 'white', textAlign: 'center', padding: 10}}>
+            Submit
+          </Text>
+        </Pressable>
       </ScrollView>
     </SafeAreaView>
   );
